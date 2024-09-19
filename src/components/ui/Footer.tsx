6e5c381c20ae027eddddb2cbf2a865a943bc2ea1
Anthony Ferreira <anthonyferreiraminard@gmail.com>
2024-09-19 20:47:58 +0200

    💄 US1: HOME > Footer > Add Style

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,3 +1,19 @@
+import styled from "styled-components";
+import { theme } from "../../theme/Theme";
+
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <FooterStyled>
+      <p>Un zoo 100% écologique et indépendant énergétiquement</p>
+      <p>© 2024 Zoo Arcadia - Tous droits réservés</p>
+    </FooterStyled>
+  );
 }
+
+const FooterStyled = styled.footer`
+  background: ${theme.colors.primary};
+  color: white;
+  text-align: center;
+  align-content: center;
+  padding: 2rem 0;
+`;
